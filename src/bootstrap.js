af3cad0596e5ed5effffe251da0dbacf4ab63a2a
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2024-07-25 11:26:45 -0400

    Comment out Amplify in bootsrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,8 +6,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from 'aws-amplify';
-import amplify from './aws-exports';
-Amplify.configure(amplify);
+//import amplify from './aws-exports';
+//Amplify.configure(amplify);
 
 const mount = (el, {onNavigate, defaultHistory, initialPath}) => {
   const history =
